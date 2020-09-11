bae28cae7227d86a45f47870ecd22830bd6bf021
Sowmya R <sowmyas@baasha.net>
2020-09-11 15:47:42 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import * as JsBarcode from 'jsbarcode';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'uuidApp';
   newUUID = '';
   referenceLink = [
@@ -24,10 +24,10 @@ export class AppComponent implements OnInit{
   constructor() {
   }
   ngOnInit() {
-    
+
     this.generateNewUUID();
   }
-  
+
   generateNewUUID() {
     this.newUUID = uuidv4();
     JsBarcode.default('#barcode', this.newUUID, {
@@ -36,6 +36,15 @@ export class AppComponent implements OnInit{
       height: 40,
       displayValue: true
     });
+    JsBarcode.default('#ean-13', '1234567890128', { format: 'ean13', height: 40 });
+    JsBarcode.default('#ean-8', '12345670', { format: 'ean8', height: 40 });
+    JsBarcode.default('#ean-5', '12345', { format: 'ean5', height: 40 });
+    JsBarcode.default('#ean-2', '12', { format: 'ean2', height: 40 });
+    JsBarcode.default('#upc-a', '123456789012', { format: 'upc', height: 40 });
+    JsBarcode.default('#code39', 'Hello', { format: 'code39', height: 40 });
+    JsBarcode.default('#itf-14', '1234567890123', { format: 'itf14', height: 40 });
+    JsBarcode.default('#msi', '123456', { format: 'msi', height: 40 });
+    JsBarcode.default('#pharmacode', '12345', { format: 'pharmacode', height: 40 });
   }
 }
 
